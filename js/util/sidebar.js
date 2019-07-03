255768dda398213af2fadbcd674a3b2837f80c95
Zeynab mohammadTabar <zeynab.mohammadtabar@gmil.com>
2019-07-03 23:34:02 +0430

    new sidebar test

--- js/util/sidebar.js
+++ js/util/sidebar.js
@@ -1,10 +1,42 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-}
+//function w3_open() {
+//    document.getElementById("mySidebar").style.display = "block";
+//    document.getElementById("myNav").style.display = "block";
+//}
+//
+//function w3_close() {
+//    document.getElementById("mySidebar").style.display = "none";
+//
+//}
+//
+//
+//
 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-}
 
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
+    trigger.click(function () {
+        hamburger_cross();
+    });
 
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
