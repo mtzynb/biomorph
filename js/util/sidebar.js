ac22b28d583c4892c73fd8242288e2ec74cec962
Zeynab mohammadTabar <zeynab.mohammadtabar@gmil.com>
2019-07-04 01:02:35 +0430

    magnifiy sidebar-TEST

--- js/util/sidebar.js
+++ js/util/sidebar.js
@@ -1,17 +1,3 @@
-//function w3_open() {
-//    document.getElementById("mySidebar").style.display = "block";
-//    document.getElementById("myNav").style.display = "block";
-//}
-//
-//function w3_close() {
-//    document.getElementById("mySidebar").style.display = "none";
-//
-//}
-//
-//
-//
-
-
 $(document).ready(function () {
     var trigger = $('.hamburger'),
         overlay = $('.overlay'),
